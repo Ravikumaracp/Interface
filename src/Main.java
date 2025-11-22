7962d9aa25a9ec051b145f692caf660837363680
Ravikumara C P <111078786+Ravikumaracp@users.noreply.github.com>
2025-11-22 11:05:44 +0530

    Fix typos and enhance comments in Main.java
    Corrected typos and improved clarity in comments.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 //Interface
 /*interface in java is a blueprint of a class or part of a class
 when class having a only abstract methods then that time we will creats interface
-by defalut all methods in interface are public abstract, if mention also no problem
+by defalut all methods in interface are public abstract, if we didn'tmention also no problem
 once if you  creates concrete class you must implement abstract method, if you fail by default concrete class will be abstract
 we can create variables in interface but the condition is everytype variable is final and static by default.
 that means we need to defines the value to that variable and we can't mofify the variable.
